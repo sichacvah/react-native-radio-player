465d291d72ad4272244241d13054a0f066491502
Sichacvah <sichirc@gmail.com>
2016-11-29 14:52:15 +0500

    fix js task

--- radioPlayerTask.js
+++ radioPlayerTask.js
@@ -2,6 +2,8 @@ import RadioPlayer from './index'
 module.exports = async ({radioPath, action}) => {
   switch(action) {
       case "STARTFOREGROUND_ACTION":
+        RadioPlayer.start(radioPath);
+        break;
       case "PLAY_ACTION":
         RadioPlayer.toggle(radioPath);
         break;
